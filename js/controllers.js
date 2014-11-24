1bb6d7c1059f907836f55216322d7df0302654eb
agustinbaretto <agustinbaretto@gmail.com>
2014-11-24 21:30:23 +0000

    lala

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', ['ionic'])
 .constant('FORECASTIO_KEY', '79a4cc625c1b8c577a215fa9ae661ded')
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  alert("hi!");
+  
   //Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
@@ -117,6 +117,7 @@ angular.module('starter.controllers', ['ionic'])
     });
     $scope.modal.hide();
   };
+  alert("ho!");
 })
 
 ////////////////////////////////////////////////////////////////////////////////
